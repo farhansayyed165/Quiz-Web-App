193915a2bcd0fdc61fa31ba452c8ae9af5a09f28
farhansayyed165 <farhansayyed1656@gmail.com>
2023-01-20 20:48:33 +0530

    Added Start Button and Added Submit Button(Submit button not working)

--- quiz.js
+++ quiz.js
@@ -4,20 +4,24 @@ const track = document.querySelector('.AllTemplates');
 const slides = Array.from(track.children);
 const next = document.querySelector('.next');
 const prev = document.querySelector('.previous');
+const submit = document.querySelector('.Submit');
+const form = document.querySelector("#form");
+
+const start = document.querySelector(".startDiv");
+const startButton = document.getElementById("start");
 
 //Getting Progress Bar and the Base for calculations
 const progress = document.querySelector('.carousel-progress-bar');
 const base = document.querySelector('.base');
-const lastlast = slides[slides.length-2]
-console.log('haha');
-console.log(lastlast);
+
 
 
 //Calculating what width the Progress Bar will have depending on the child elements i.e. Number of Questions
 const baseWidth = base.offsetWidth;
-const addToProgress =  (baseWidth/slides.length-1); //Important Constant we will use in the Eventlisteners
+const addToProgress =  (baseWidth/(slides.length-1));//Important Constant we will use in the Eventlisteners
+console.log(addToProgress);
 let progressW = progress.offsetWidth;
-progress.style.width = addToProgress+'px';
+progress.style.width = 0+'px';
 console.log(`BaseWidth ${baseWidth}`);
 
 
@@ -26,8 +30,6 @@ console.log(`BaseWidth ${baseWidth}`);
 // Now, actual coding
 
 // Getting Width of the template
-const sWidth = slides[0].getBoundingClientRect().width;
-console.log(sWidth);
 let i = slides.length;
 
 
@@ -39,11 +41,27 @@ const setSlidePosition = (slide, index)=>{
 
 slides.forEach((slide, index)=>{
     slide.style.left = 100 * index + '%';
-    console.log(slide);
+    // console.log(slide);
 });
 
 
 // Adding  Eventlisteners that will slide questions as well as modify the progress bar 
+
+startButton.addEventListener('click',_e=>{
+    startButton.style.display='none';
+    const currentSlide = track.querySelector('.current');
+    const nextSlide = currentSlide.nextElementSibling;
+    moveToSlide(track,currentSlide,nextSlide);
+
+
+    let progressWidth = progress.offsetWidth;
+    progress.style.width =progressWidth + (addToProgress)+'px';
+    document.querySelector(".option_footer").style.display="flex";
+    prev.style.display="none";
+});
+
+
+
 
 function moveToSlide(track, currentSlide, targetSlide){
     track.style.transform = 'translateX(-'+ targetSlide.style.left +')';
@@ -53,19 +71,27 @@ function moveToSlide(track, currentSlide, targetSlide){
 
 
 
+const lastlast = slides[slides.length-2];
+const firstSlide = slides[1];
+const SecondSlide = slides[2];
+console.log(slides[(slides.length-2)]);
+
 next.addEventListener('click',e=>{
     const currentSlide = track.querySelector('.current');
     const nextSlide = currentSlide.nextElementSibling;
-    const amountToMove = nextSlide.style.left;
+    // const amountToMove = nextSlide.style.left;
     moveToSlide(track,currentSlide,nextSlide);
 
     // For Progress bar width changes 
-    // if (currentSlide !== document.getElementsByClassName('Template').firstChild){
-        
-    // }
     if(currentSlide === lastlast){
         progress.style.width = baseWidth+'px';
-        console.log("Lastone");
+        next.style.display = 'none';
+        console.log(baseWidth);
+    }
+    if(currentSlide === firstSlide){
+        let progressWidth = progress.offsetWidth;
+        progress.style.width = progressWidth + addToProgress+'px';
+        prev.style.display="inline-block";
     }
     else{
         let progressWidth = progress.offsetWidth;
@@ -81,13 +107,36 @@ prev.addEventListener('click', e=>{
     
     // For Progress bar width changes 
     let progressWidth = progress.offsetWidth;
+    if(currentSlide === slides[slides.length-1]){
+        progress.style.width = progressWidth - addToProgress+'px';
+        next.style.display = 'inline-block';
+    }
     if (currentSlide === document.getElementsByClassName('Template').firstChild){
+        
         progress.style.width = addToProgress;
+    }
+    if(currentSlide === SecondSlide){
+        progress.style.width = progressWidth - addToProgress+'px';
+        prev.style.display = 'none';
     }
     else{
     progress.style.width = progressWidth - addToProgress+'px';
     }
 
 })
+
+
+
+form.addEventListener("submit",submitHandle)
+
+function submitHandle(event){
+    event.preventDefault();
+
+}
+
+
+
+
+
 
 
