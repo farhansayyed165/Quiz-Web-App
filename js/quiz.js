711a6fea051cbf1ed8ba311a924c2811e0aeb4bd
farhansayyed165 <farhansayyed1656@gmail.com>
2023-01-25 21:30:49 +0530

    organized everything and fixed progress bar!

--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,9 @@
 const content = [
-    {question:"1. Name the lightest gas on earth.", a:"Oxygen",b:"Hydrogen",c:"Nitrogen",d:"Helium", answer:"Hydrogen"},
-    {question:"2. What is the largest desert in the world?", a:"Antarctica",b:"Sahara",c:"Arabian Desert",d:"Gobi Desert", answer:"Antarctica"},
-    {question:"3. What is a material that will not carry an electrical charge called?", a:"Conductor",b:"Semicondoctor",c:"Insulator",d:"None of the above", answer:"Insulator"},
-    {question:"4. Roughly how long does it take for the Sun’s light to reach Earth?", a:"8 Days",b:"8 Hours",c:"8 minutes",d:"8 seconds", answer:"8 minutes"},
-    {question:"5. How many bones do sharks have in their bodies?", a:"0 Bones",b:"201 Bones",c:"400 Bones",d:"98 Bones", answer:"0 Bones"}
+    {question:"2 1. Name the lightest gas on earth.", a:"Oxygen",b:"Hydrogen",c:"Nitrogen",d:"Helium", answer:"Hydrogen"},
+    {question:"3 2. What is the largest desert in the world?", a:"Antarctica",b:"Sahara",c:"Arabian Desert",d:"Gobi Desert", answer:"Antarctica"},
+    {question:"4 3. What is a material that will not carry an electrical charge called?", a:"Conductor",b:"Semicondoctor",c:"Insulator",d:"None of the above", answer:"Insulator"},
+    {question:"5 4. Roughly how long does it take for the Sun’s light to reach Earth?", a:"8 Days",b:"8 Hours",c:"8 minutes",d:"8 seconds", answer:"8 minutes"},
+    {question:"6 5. How many bones do sharks have in their bodies?", a:"0 Bones",b:"201 Bones",c:"400 Bones",d:"98 Bones", answer:"0 Bones"}
 ]
 
 const questions = document.querySelectorAll(".question");
@@ -84,8 +84,7 @@ startButton.addEventListener('click',_e=>{
     moveToSlide(track,currentSlide,nextSlide);
 
 
-    let progressWidth = progress.offsetWidth;
-    progress.style.width =progressWidth + (addToProgress)+'px';
+    progress.style.width = (addToProgress)+'px';
     document.querySelector(".option_footer").style.display="flex";
     prev.style.display="none";
 });
@@ -104,33 +103,42 @@ function moveToSlide(track, currentSlide, targetSlide){
 const lastlast = slides[slides.length-2];
 const firstSlide = slides[1];
 const SecondSlide = slides[2];
-console.log(slides[(slides.length-2)]);
 
+
+let nextMult = 2;
+let prevMult = nextMult-1
 next.addEventListener('click',e=>{
     const currentSlide = track.querySelector('.current');
     const nextSlide = currentSlide.nextElementSibling;
-    // const amountToMove = nextSlide.style.left;
     moveToSlide(track,currentSlide,nextSlide);
 
     // For Progress bar width changes 
     if(currentSlide === lastlast){
-        progress.style.width = baseWidth+'px';
+        progress.style.width = nextMult * addToProgress+'px';
         next.style.display = 'none';
         // submit.setAttribute("visible","true");
+
     }
     if(currentSlide === firstSlide){
-        let progressWidth = progress.offsetWidth;
-        progress.style.width = progressWidth + addToProgress+'px';
+        // let progressWidth = progress.offsetWidth;
+        progress.style.width = nextMult * addToProgress+'px';
+        nextMult++;
+        prevMult++;
         prev.style.display="inline-block";
     }
     else{
-        let progressWidth = progress.offsetWidth;
-        progress.style.width = progressWidth + addToProgress+'px';
+        // let progressWidth = progress.offsetWidth;
+        progress.style.width = nextMult*addToProgress+'px';
+        nextMult++;
+        prevMult++;
     }
     
 
 
 })
+
+
+
 
 prev.addEventListener('click', e=>{
     const currentSlide = track.querySelector('.current');
@@ -138,47 +146,55 @@ prev.addEventListener('click', e=>{
     moveToSlide(track,currentSlide,nextSlide);
     
     
-    // For Progress bar width changes 
-    let progressWidth = progress.offsetWidth;
+    // For Progress bar width changes
+
     if(currentSlide === slides[slides.length-1]){
-        progress.style.width = progressWidth - addToProgress+'px';
+        progress.style.width = prevMult*addToProgress+'px';
         next.style.display = 'inline-block';
+
     }
-    if (currentSlide === document.getElementsByClassName('Template').firstChild){
-        
-        progress.style.width = addToProgress;
-    }
+
+    //set
     if(currentSlide === SecondSlide){
-        progress.style.width = progressWidth - addToProgress+'px';
-        prev.style.display = 'none';
+        nextMult--;
+        prevMult--;
+        progress.style.width = addToProgress+'px';
+        prev.style.display="none";
+
     }
     else{
-    progress.style.width = progressWidth - addToProgress+'px';
+        nextMult--;
+        prevMult--;
+        progress.style.width = prevMult*addToProgress+'px';
+
     }
 
 })
 
 yes.addEventListener('click',()=>{
     submit.setAttribute("visible","true");
-    prev.style.display = 'none';
+    // prev.style.display = 'none';
+    prev.setAttribute("visible", "false")
     yes.style.display = 'none';
     no.innerHTML = "Go Back";
 })
 
 no.addEventListener('click',()=>{
+    submit.setAttribute("visible","false");
     const currentSlide = track.querySelector('.current');
     const nextSlide = currentSlide.previousElementSibling;
     moveToSlide(track,currentSlide,nextSlide);
-
-    let progressWidth = progress.offsetWidth;
-    progress.style.width = progressWidth - addToProgress+'px';
+    prevMult--;
+    nextMult--;
+    progress.style.width = prevMult*addToProgress+'px';
     next.style.display = 'inline-block';
-
-    submit.setAttribute("visible","false");
-    prev.style.display = 'inline-block';
+    prev.setAttribute("visible", "true")
     yes.style.display = 'inline-block';
     no.innerHTML = "No";
 })
+
+
+
 
 
 form.addEventListener("submit",submitHandle)
@@ -187,14 +203,25 @@ function submitHandle(event){
     event.preventDefault();
     radio = document.querySelectorAll("input[type='radio']");
     checked = document.querySelectorAll("li input[type='radio']:checked + label");
-    if (checked.length<content.length){
-        return alert("Please Select all the Options!");
-    }
+    // if (checked.length<content.length){
+    //     return alert("Please choose one answer from each question!");
+    // }
     let marks_scored=0;
     for(let i = 0; i<=(content.length-1);i++){
         if(checked[i].innerHTML===content[i].answer){
             marks_scored = marks_scored+1;
         }
+    }
+
+    if(marks_scored<=2){
+        document.querySelector(".ScoreBefore").innerHTML = "Better Luck Next Time :(";
+    }
+    if(marks_scored===checked.length){
+        document.querySelector(".ScoreBefore").innerHTML = "🥳You did it! Full marks";
+    }
+    else{
+        document.querySelector(".ScoreBefore").innerHTML = "You did well!👍";
+
     }
 
     let final_score = marks_scored+" out of "+checked.length+"!"
@@ -203,9 +230,6 @@ function submitHandle(event){
     let final_screen = document.querySelector(".thank_you")
     base.classList.add("blur");
     final_screen.classList.add("visible")
-
-    
-
 }
 
 
